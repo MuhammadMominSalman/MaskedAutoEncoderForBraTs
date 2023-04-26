be0e1df7e763adabc5fc8c041350e1707c5ab794
MuhammadMominSalman <109581390+MuhammadMominSalman@users.noreply.github.com>
2023-04-26 13:36:37 +0200

    Add files via upload

--- models_vit.py
+++ models_vit.py
@@ -13,16 +13,28 @@ from functools import partial
 
 import torch
 import torch.nn as nn
+from PatchEmbed import PatchEmbed
 
-import timm.models.vision_transformer
+from timm.models.vision_transformer import Block
 
 
-class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
+class VisionTransformer(nn.Module):
     """ Vision Transformer with support for global average pooling
     """
-    def __init__(self, global_pool=False, **kwargs):
-        super(VisionTransformer, self).__init__(**kwargs)
+    def __init__(self, global_pool=False, img_size=128, patch_size=16, in_chans=4, embed_dim=1024, depth=24, num_heads=16,
+                 norm_layer=nn.LayerNorm, mlp_ratio=4., **kwargs):
+        super().__init__()
+        frame_patch_size = 16
+        self.patch_embed = PatchEmbed(frames=128, frame_patch_size=frame_patch_size, dim=embed_dim, img_size=img_size,
+                                      patch_size=patch_size, channels=in_chans)
+        num_patches = self.patch_embed.num_patches
 
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.randn(1, num_patches + 1, embed_dim))
         self.global_pool = global_pool
         if self.global_pool:
             norm_layer = kwargs['norm_layer']
@@ -31,27 +43,24 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
 
             del self.norm  # remove the original norm
 
-    def forward_features(self, x):
-        B = x.shape[0]
+    def forward(self, x):
+
         x = self.patch_embed(x)
 
-        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
+        cls_tokens = cls_token.expand(x.shape[0], -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
-        x = x + self.pos_embed
-        x = self.pos_drop(x)
-
-        for blk in self.blocks:
+        # x = self.pos_drop(x)
+        ext_layers = [3, 6, 9, 12]
+        extract_layers = []
+        for depth, blk in enumerate(self.blocks):
             x = blk(x)
+            if depth + 1 in ext_layers:
+                extract_layers.append(x)
 
-        if self.global_pool:
-            x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
-            outcome = self.fc_norm(x)
-        else:
-            x = self.norm(x)
-            outcome = x[:, 0]
-
-        return outcome
-
+        return extract_layers
 
 def vit_base_patch16(**kwargs):
     model = VisionTransformer(
